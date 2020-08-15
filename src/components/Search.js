379161ce058bcb22faacaa36fca13ea21af4a845
Shrfu31 <Shrfu31@Mac-Pro.localdomain>
2020-08-15 18:50:16 -0400

    Introduced useEffect state hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const Search = () => {
-  return <h1> Search</h1>;
+  const [term, setTerm] = useState("");
+
+  console.log("I run with every render");
+  //useEffect for rerendering when code detects that 'term' has changed
+  //doesn't have to be arrow functions
+  //use effect will only run in one of 3 scenarios. it is the second argument that shows when it gets affected
+  //it is blank array, some array with elements, or no array at all
+  useEffect(() => {
+    console.log("I  run at initial render and every render");
+  }, [term]);
+  return (
+    <div>
+      <div className="ui form">
+        <div className=" field">
+          <label> Enter Search Term </label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Search;
